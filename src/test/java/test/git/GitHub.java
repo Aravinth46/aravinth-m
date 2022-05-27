84f7b72f08b5122b32873f4765b451389e0b2ab9
aravinth46 <aravind956626@gmail.com>
2022-05-27 12:02:31 +0530

    commit message

---
+++ src/test/java/test/git/GitHub.java
@@ -0,0 +1,15 @@
+package test.git;
+
+public class GitHub {
+	
+	private void m1() {
+System.out.println("m1");
+	}
+	private void m3() {
+		System.out.println("m3");
+	}
+	private void m2() {
+		System.out.println("m2");
+	}
+
+}
